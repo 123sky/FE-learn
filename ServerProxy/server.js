cb66853defa2550bcff0d9aff98a08fe00e29e5f
chenjp <chenjianpeng@utry.cn>
2018-05-18 15:00:07 +0800

    init

---
+++ ServerProxy/server.js
@@ -0,0 +1,24 @@
+const url = require('url');
+const http = require('http');
+const https = require('https');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json; charset=utf-8'
+  });
+  const path = url.parse(req.url).path.slice(1);
+  if (path === 'topics') {
+    https.get('https://cnodejs.org/api/v1/topics', (resp) => {
+      let data = "";
+      resp.on('data', chunk => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        res.end(data);
+      });
+    })
+  }
+}).listen(3000, '127.0.0.1');
+
+console.log('启动服务，监听 127.0.0.1:3000');
